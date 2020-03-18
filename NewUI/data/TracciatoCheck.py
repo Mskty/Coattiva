221da4d5eb7ba3974c2027f1195e7701c85b9d59
Matteo <Squeri1997@gmail.com>
2020-03-18 21:00:54 +0100

    commenti sulla classe di controllo, teoricamente terminata

--- NewUI/data/TracciatoCheck.py
+++ NewUI/data/TracciatoCheck.py
@@ -3,6 +3,29 @@ from utility.Enums import *
 
 
 class TracciatoCheck:
+    """
+    Classe che fornisce il metodo checkcolumns per verificare che non ci siano errori nelle colonne presenti
+    in un dataframe generato da file .csv Nel caso siano presenti errori solleva un eccezione di tipo ValueError che
+    dovrà essere gestita (viene gestita nella view informando l'utente degli errori presenti nel file)
+    PARAMETRI:
+    self.df: oggetto di tipo Pandas Dataframe contenente i dati da analizzare
+    self.typepfpf: Valore di tipo PFPGEnum rappresentante il tipo di dati che dovrebbero essere contenuti in self.df
+    self.filetype: Valore di tipo NewFileEnum rappresentante il tipo di file csv che dovrebbe essere stato caricato
+                   da cui sono derivati i dati contenuti in self.df
+    self.oldcolumns: Lista dei nomi delle colonne come da tracciato per file riferiti a dati di titoli credito storici
+                     non aggregati
+    self.newPFcolumns: Lista dei nomi delle colonne come da tracciato per file riferiti a dati recenti di titoli
+                       di credito aggregati riferiti a persone fisiche
+    self.newPGcolumns: Lista dei nomi delle colonne come da tracciato per file riferiti a dati recenti di titoli
+                       di credito aggregati riferiti a persone giuridiche
+    """
+
+    """
+        @PRE nella descrizione dei metodi si riferisce alla precondizione che deve essere soddisfatta prima dell'invocazione
+            di tale metodo da parte dell'utente, tra le precondizioni è sempre considerata soddisfatta la creazione dell'oggetto
+            e l'invocazione di __init__
+    """
+
     def __init__(self, typepfpg: PFPGEnum, filetype: NewFileEnum, df: pd.DataFrame):
         self.df = df.copy()
         self.typepfpg = typepfpg
@@ -27,6 +50,12 @@ class TracciatoCheck:
                              "Vetusta", "TitoliCredito"]
 
     def checkcolumns(self):
+        """
+        @PRE: nessuna
+        Effettua controlli sulla correttezza delle colonne presenti nel dataframe self.df. Nel caso siano individuati
+        errori ferma l'esecuzione del metodo e solleva un eccezione ValueError contentente la descrizione dell'errore.
+        :return: None
+        """
         columns = list(self.df.columns.values)
         # Controllo che le colonne in columns siano esattamente quelle che devono essere
 
